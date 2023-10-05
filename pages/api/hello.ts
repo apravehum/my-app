7ca3e1e0713695d18a1c886f6c018a889fb58230
Ernests <ernests.gorns@ecenta.com>
2023-10-05 21:40:46 +0300

    Adding query parameter

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const {ernests} = req.query;
+  if (Array.isArray(ernests) || !ernests) {
+    throw new Error("This is an array or undefined");
+  }
+
+  res.status(200).json({ name: ernests.toUpperCase() });
 }
